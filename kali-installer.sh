4c911d015f73974b1426e8ec559aeb8d6a1d5ed9
KiLAN01 <70495592+KiLAN01@users.noreply.github.com>
2021-05-17 20:04:00 -0400

    Update kali-installer.sh

--- kali-installer.sh
+++ kali-installer.sh
@@ -11,21 +11,7 @@ echo -e '\033[1;31m
 ·▀▀▀▀   ▀ •   ·▀  ▀▀▀▀.▀▀▀  ▀  ▀ ▀▀ █▪    
 \033[1;31m'
 echo -e '\033[0;33m warning: every time you start termux it will start kali, and for you to exit kali, and go to the termux terminal type exit \033[0m'
-sleep 1.5
-clear
-echo -e '\033[1;31m [+] installing-lolcat [+] \033[1;31m'
-apt install ruby -y > /dev/null 2>&1
-apt install wget -y > /dev/null 2>&1
-wget https://github.com/busyloop/lolcat/archive/master.zip > /dev/null 2>&1
-apt install unzip -y > /dev/null 2>&1
-unzip master.zip > /dev/null 2>&1
-cd lolcat-master/bin
-gem install lolcat > /dev/null 2>&1
-cd
-sleep 1
-clear
-echo -e '\033[1;31m [+] successfully-installed [+] \033[1;31m'
-sleep 1.5
+sleep 3
 clear
 #|--------------------------------credits--------------------------------|
 echo -e '\033[1;31m
@@ -36,16 +22,16 @@ echo -e '\033[1;31m
 ·▀▀▀ .▀  ▀ ▀▀▀ ▀▀▀▀▀• ▀▀▀ ▀▀▀  ▀▀▀▀ 
 \033[1;31m'
 echo -e '\033[1;31m program developed by kilan01 youtube: https://www.youtube.com/channel/UCaVBnB4CQxiW6-Nuhmi1gqa \033[1;31m'
-sleep 1.5
+sleep 1
 clear
 #|--------------------------------installing-package--------------------------------|
 echo -e '\033[0;33m [+] installing packages... [+] \033[0;33m'
-apt upgrade && apt update -y > /dev/null 2>&1
-apt install wget -y > /dev/null 2>&1
-sleep 1.5
+apt upgrade && apt update -y 
+apt install wget -y 
+sleep 1
 clear
 echo -e '\033[0;33m [+]installed-packages[+] \033[0;33m'
-sleep 1.5
+sleep 1
 clear
 #|--------------------------------initialized--------------------------------|
 echo -e '\033[1;31m
@@ -55,15 +41,16 @@ echo -e '\033[1;31m
 ▐█.█▌▐█▪ ▐▌▐█▌ ▄▐█▌  ▐█▌██▐█▌▐█▄▪▐█ ▐█▌·▐█▪ ▐▌▐█▌ ▄▐█▌ ▄▐█▄▄▌▐█•█▌
 ·▀  ▀ ▀  ▀ .▀▀▀ ▀▀▀  ▀▀▀▀▀ █▪ ▀▀▀▀  ▀▀▀  ▀  ▀ .▀▀▀ .▀▀▀  ▀▀▀ .▀  ▀
 \033[1;31m'
-sleep 1.5
+sleep 1
 echo -e "\033[1;31m warning: this installation process takes a long time so be patient \033[1;31"
-pkg update -y && pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Kali/kali-xfce.sh | bash
-cd data/data/com.termux/files/usr/etc
-rm -rf motd
-rm -rf bash.bashrc
-cp bash.bashrc $HOME
-cd  $HOME
-mv bash.bashrc /data/data/com.termux/files/usr/etc
+clear
+    cp bash.bashrc $HOME
+    cd /data/data/com.termux/files/usr/etc
+    rm -rf motd
+    rm -rf bash.bashrc
+    cd $HOME
+    mv bash.bashrc /data/data/com.termux/files/usr/etc
+pkg update -y && pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Kali/kali-xfce.sh
 clear
 echo -e '\033[1;31m
 ▄▄▄▄·  ▄· ▄▌  ▄ •▄ ▪  ▄▄▌   ▄▄▄·  ▐ ▄     
@@ -73,4 +60,3 @@ echo -e '\033[1;31m
 ·▀▀▀▀   ▀ •   ·▀  ▀▀▀▀.▀▀▀  ▀  ▀ ▀▀ █▪    
 \033[1;31m'
 echo -e '\033[1;31m program finished restart your termux to initialize kali \033[1;31m'
-fi
